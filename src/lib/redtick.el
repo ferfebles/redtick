66d26021e3bed15e0bb292d5c683380944507943
ferfebles <ferfebles@gmail.com>
2016-03-27 15:15:23 +0100

    Looots of changes... Code Working!!!

--- src/lib/redtick.el
+++ src/lib/redtick.el
@@ -1,29 +1,33 @@
-;; pomodoro work & break intervals
+;; pomodoro work & break intervals in seconds
 (defvar redtick-work-interval (* 6 25))
 (defvar redtick-break-interval (* 6 5))
-;; pomodoro start time
-(defvar redtick-started-at (float-time))
+
 ;; redtick bars for every interval
 (defvar redtick-workbars-interval (/ redtick-work-interval 8.0))
 (defvar redtick-breakbars-interval (/ redtick-break-interval 8.0))
+
+;; bar & colours http://www.utexas.edu/learn/html/colors.html
 (defvar redtick-bars
-  '((redtick-workbars-interval "█" "#ffff66")
-   (redtick-workbars-interval "▇" "#ffcc66")
-   (redtick-workbars-interval "▆" "#cc9966")
-   (redtick-workbars-interval "▅" "#ff9966")
-   (redtick-workbars-interval "▄" "#cc6666")
-   (redtick-workbars-interval "▃" "#ff6666")
-   (redtick-workbars-interval "▂" "#ff3366")
-   (redtick-workbars-interval "▁" "#ff0066")
-   (redtick-breakbars-interval "█" "#00cc66")
-   (redtick-breakbars-interval "▇" "#33cc66")
-   (redtick-breakbars-interval "▆" "#66cc66")
-   (redtick-breakbars-interval "▅" "#00ff66")
-   (redtick-breakbars-interval "▄" "#33ff66")
-   (redtick-breakbars-interval "▃" "#66ff66")
-   (redtick-breakbars-interval "▂" "#99ff66")
-   (redtick-breakbars-interval "▁" "#ccff66")
-   ("∞" "SkyBlue2" nil)))
+  '((4 "█" "#ffff66") ;; 4 -> redtick-workbars-interval
+    (4 "▇" "#ffcc66")
+    (4 "▆" "#cc9966")
+    (4 "▅" "#ff9966")
+    (4 "▄" "#cc6666")
+    (4 "▃" "#ff6666")
+    (4 "▂" "#ff3366")
+    (4 "▁" "#ff0066")
+    (1 "█" "#00cc66")  ;; 1 -> redtick-breakbars-interval
+    (1 "▇" "#33cc66")
+    (1 "▆" "#66cc66")
+    (1 "▅" "#00ff66")
+    (1 "▄" "#33ff66")
+    (1 "▃" "#66ff66")
+    (1 "▂" "#99ff66")
+    (1 "▁" "#ccff66")
+    (nil "✓" "SkyBlue2")))
+
+;; pomodoro start time
+(defvar redtick-started-at (float-time))
 
 ;; seconds since pomodoro started
 (defun redtick-seconds-since-started ()
@@ -45,74 +49,76 @@
               'pointer 'hand
               'local-map (make-mode-line-mouse-map
                           'mouse-1 (lambda () (interactive)
-                                       (setq redtick-started-at (float-time))
+                                     (redtick-start)
                                      ))))
 
-(defun redtick-workbar-interval (i)
-  (/ (* i redtick-work-interval) 8))
-(defun redtick-breakbar-interval (i)
-  (+ redtick-work-interval (/ (* i redtick-break-interval) 8)))
+(defvar redtick-selwin (selected-window))
 
-;; how to appy a funtion to a list
-;; (setq redtick-current-bars redtick-bars)
-;; (setq current-bar (pop redtick-current-bars))
-;; (setq current-time (pop current-bar))
-;; (message current-bar)
-;; (apply 'redtick-propertize current-bar)
-
-;; draws bar http://www.utexas.edu/learn/html/colors.html
-(defun redtick-bar (seconds)
-  (cond
-   ((< seconds (redtick-workbar-interval 1)) (redtick-propertize "█" "#ffff66"))
-   ((< seconds (redtick-workbar-interval 2)) (redtick-propertize "▇" "#ffcc66"))
-   ((< seconds (redtick-workbar-interval 3)) (redtick-propertize "▆" "#cc9966"))
-   ((< seconds (redtick-workbar-interval 4)) (redtick-propertize "▅" "#ff9966"))
-   ((< seconds (redtick-workbar-interval 5)) (redtick-propertize "▄" "#cc6666"))
-   ((< seconds (redtick-workbar-interval 6)) (redtick-propertize "▃" "#ff6666"))
-   ((< seconds (redtick-workbar-interval 7)) (redtick-propertize "▂" "#ff3366"))
-   ((< seconds (redtick-workbar-interval 8)) (redtick-propertize "▁" "#ff0066"))
-   ((< seconds (redtick-breakbar-interval 1)) (redtick-propertize "█" "#00cc66"))
-   ((< seconds (redtick-breakbar-interval 2)) (redtick-propertize "▇" "#33cc66"))
-   ((< seconds (redtick-breakbar-interval 3)) (redtick-propertize "▆" "#66cc66"))
-   ((< seconds (redtick-breakbar-interval 4)) (redtick-propertize "▅" "#00ff66"))
-   ((< seconds (redtick-breakbar-interval 5)) (redtick-propertize "▄" "#33ff66"))
-   ((< seconds (redtick-breakbar-interval 6)) (redtick-propertize "▃" "#66ff66"))
-   ((< seconds (redtick-breakbar-interval 7)) (redtick-propertize "▂" "#99ff66"))
-   ((< seconds (redtick-breakbar-interval 8)) (redtick-propertize "▁" "#ccff66"))
-   (t (redtick-propertize "∞" "SkyBlue2"))))
-
-(defun redtick-redraw-mode-line ()
-  (progn
-    (force-mode-line-update)
-    (run-at-time 5 nil 'redtick-redraw-mode-line)))
-
-(defun redtick-set-selected-window ()
-  "sets the variable `redtick-selected-window` appropriately"
+(defun redtick-set-selwin (windows)
   (when (not (minibuffer-window-active-p (frame-selected-window)))
-    (setq redtick-selected-window (frame-selected-window))))
+    (setq redtick-selwin (selected-window))))
 
-(add-hook 'window-configuration-change-hook 'redtick-set-selected-window)
-(add-hook 'focus-in-hook 'redtick-set-selected-window)
-(add-hook 'focus-out-hook 'redtick-set-selected-window)
+(add-function :before pre-redisplay-function #'redtick-set-selwin)
 
-(defadvice select-window (after redtick-select-window activate)
-  "makes redtick aware of window changes"
-  (redtick-set-selected-window))
 
-(defun redtick-selected-window-active ()
-  "Return whether the current window is active."
-  (eq redtick-selected-window (selected-window)))
+;; (setq redtick-timer nil)
 
-(defun redtick-bar-in-selected-window ()
-  (if (redtick-selected-window-active)
-      (redtick-bar (redtick-seconds-since-started))))
+(defvar redtick-current-bar
+  (redtick-propertize "✓" "SkyBlue2"))
+(put 'redtick-current-bar 'risky-local-variable t)
+(add-to-list 'mode-line-misc-info
+             '(:eval (if (eq redtick-selwin (get-buffer-window))
+                         redtick-current-bar)))
+
+(defun redtick-update-current-bar (redtick-current-bars)
+  (progn
+    (setq redtick-current-bar (apply #'redtick-propertize
+                                     (cdar redtick-current-bars)))
+    (if (caar redtick-current-bars)
+        (run-at-time (caar redtick-current-bars)
+                     nil
+                     #'redtick-update-current-bar
+                     (cdr redtick-current-bars))
+      )
+    (force-mode-line-update t)
+    (message redtick-current-bar)
+    )
+  )
 
 (defun redtick-start ()
   (interactive)
   (progn
-    (add-to-list 'mode-line-misc-info
-                 '(:eval (redtick-bar-in-selected-window)))
-    (redtick-set-selected-window)
     (setq redtick-started-at (float-time))
+    (redtick-update-current-bar redtick-bars)
+    ;; (add-to-list 'mode-line-misc-info
+    ;;              '(redtick-current-bar redtick-current-bar))
+    ;; (redtick-set-selected-window)
     (force-mode-line-update t))
   )
+
+;; (defun redtick-redraw-mode-line ()
+;;   (progn
+;;     (force-mode-line-update)
+;;     (run-at-time 1 nil 'redtick-redraw-mode-line)))
+
+;; (defun redtick-set-selected-window ()
+;;   "sets the variable `redtick-selected-window` appropriately"
+;;   (when (not (minibuffer-window-active-p (frame-selected-window)))
+;;     (setq redtick-selected-window (frame-selected-window))))
+
+;; (add-hook 'window-configuration-change-hook 'redtick-set-selected-window)
+;; (add-hook 'focus-in-hook 'redtick-set-selected-window)
+;; (add-hook 'focus-out-hook 'redtick-set-selected-window)
+
+;; (defadvice select-window (after redtick-select-window activate)
+;;   "makes redtick aware of window changes"
+;;   (redtick-set-selected-window))
+
+;; (defun redtick-current-window-active-p ()
+;;   "Return whether the current window is active."
+;;   (eq redtick-selected-window (selected-window)))
+
+;; (defun redtick-bar-in-selected-window ()
+;;   (if (redtick-current-window-active-p)
+;;       redtick-current-bar))
+
